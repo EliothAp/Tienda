6e80a971e70fcd9928b8c43d828bf80f401a9c0a
EliothAp <eartavia70064@ufide.ac.cr>
2026-02-16 15:55:32 -0600

    Codigo actualizado semana 4 completo, pagina web abre categorias pero no funcionan los botones

--- src/main/java/com/tienda/StorageConfig.java
+++ src/main/java/com/tienda/StorageConfig.java
@@ -21,20 +21,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-public class StorageConfig {
-
+public class StorageConfig 
+{
     @Value("${firebase.json.path}")
     private String jsonPath;
-
+    
     @Value("${firebase.json.file}")
     private String jsonFile;
-
+    
     @Bean
     public Storage storage() throws IOException {
         ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
-        try (InputStream inputStream = resource.getInputStream()) {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
-            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-        }
-    }
+        try (InputStream inputStream = resource.getInputStream())
+        {
+            GoogleCredentials credencials = GoogleCredentials.fromStream(inputStream);
+            return StorageOptions.newBuilder().setCredentials(credencials).build().getService();                       
+        }        
+    }    
 }
--- src/main/resources/static/js/rutinas.js
+++ src/main/resources/static/js/rutinas.js
@@ -1,8 +1,8 @@
-// funcion para hacer un preview de una imagen 
+// función para hacer un preview de una imagen 
 function mostrarImagen(input) {
     if (input.files && input.files[0]) {
         const imagen = input.files[0];
-        const maximo = 512 * 1024; //Se limita el tamaño a 512 Kb las imágenes.
+        const maximo = 512 * 1024; // Se limita el tamaño a 512 Kb las imágenes.
         if (imagen.size <= maximo) {
             var lector = new FileReader();
             lector.onload = function (e) {
@@ -15,7 +15,7 @@ function mostrarImagen(input) {
     }
 }
 
-//Para insertar información en el modal según el registro...
+// Para insertar información en el modal según el registro...
 document.addEventListener('DOMContentLoaded', function () {
     const confirmModal = document.getElementById('confirmModal');
     confirmModal.addEventListener('show.bs.modal', function (event) {
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-//Para quitar toast
+// Para quitar toast
 setTimeout(() => {
     document.querySelectorAll('.toast').forEach(t => t.classList.remove('show'));
 }, 4000);
