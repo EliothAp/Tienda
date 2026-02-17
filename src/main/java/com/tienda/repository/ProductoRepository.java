4feaf7ac75cdf6ac3375b5bfd0e67e24f98711da
Laboratorios <Laboratorios@172.16.11.8>
2026-02-16 19:50:40 -0600

    Codigo semana 5 funcional pero no sale montos de dinero

---
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.repository;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Elioth Artavia
+ */
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+    public List<Producto> findByActivoTrue();
+}
